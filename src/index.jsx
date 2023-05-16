eeb983d783e8a1c31ca6c11c5a66c76b2b9a21aa
Dusan95z <dusan204z@gmail.com>
2023-05-16 16:34:39 +0200

    auth done

--- src/index.jsx
+++ src/index.jsx
@@ -11,11 +11,25 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain='dev-rq0hyv1n67murgjw.us.auth0.com'
+    clientId='2uMaSG0pYWy4cvC2fHq5BIrNL0NWfL3a'
+    cacheLocation='localstorage'
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
+
+// dev-rq0hyv1n67murgjw.us.auth0.com
+// 2uMaSG0pYWy4cvC2fHq5BIrNL0NWfL3a
